9b49c68b204ec899a3654773fa37a7cb8288bfbf
Jamil ETTEL <jamil.ettel@epitech.eu>
2021-12-25 14:56:41 +0000

    :construction: added args attribute to NEAT class

--- src/neat.rs
+++ src/neat.rs
@@ -4,19 +4,27 @@ use std::fmt;
 #[pyclass]
 pub struct NEAT {
     pub pop_size: i32,
+    test: i32,
 }
 
 impl fmt::Display for NEAT {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "NEAT {{ pop_size: {} }}", self.pop_size)
+        write!(f, "NEAT {{ pop_size: {}, test: {} }}", self.pop_size, self.test)
     }
 }
 
 #[pymethods]
 impl NEAT {
     #[new]
-    pub fn new(pop_size: i32) -> Self {
-        NEAT { pop_size }
+    #[args(
+        pop_size,
+        test=1,
+    )]
+    pub fn new(pop_size: i32, test: i32) -> Self {
+        NEAT {
+            pop_size,
+            test
+        }
     }
 
     fn __str__(&self) -> String {
