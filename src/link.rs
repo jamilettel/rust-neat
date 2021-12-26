c9aaab70b20628fe220edc7c7ca1bf14a4f89d2a
oktopush <oktopush>
2021-12-26 18:35:34 +0100

    :construction: added getters, setters, constuctor to Link

--- src/link.rs
+++ src/link.rs
@@ -3,32 +3,53 @@ use std::fmt;
 mod node;
 use node::Node;
 
-pub struct Link {
+pub struct Link<'a>{
     // Defines a directional Link in the neural network
-    src: Node, // Source Node
-    dst: Node, // Destination Node
+    src: &'a Node, // Source Node
+    dst: &'a Node, // Destination Node
     weight: f64, // Weight of the Link
   
 } 
 
 
-impl fmt::Display for Link { 
-    // Allows us to print the Link with the default formatter 
-    pub fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+/// Allows us to print the Link with the default formatter 
+impl<'a> fmt::Display for Link<'a> { 
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, " {} -- {} --> {} ", self.src, self.weight, self.dst)
     }
 }
 
-impl Link {
-    pub fn get_weight(&self) -> f64 {
-        self.weight 
+impl<'a> Link<'a> {
+
+    /// Creates and returns a new Link with specified arguments as attributes
+    pub fn new(src: &'a Node, dst: &'a Node, weight: f64) -> Link<'a> {
+        return Link {
+            src, 
+            dst, 
+            weight
+        }
     }
+  
+
+    pub fn get_weight(&self) -> f64 { self.weight }
         
-    pub fn get_src(&self) -> Node {
-        self.src
+    pub fn get_src(&self) -> &Node { self.src }
+
+    pub fn get_dst(&self) -> &Node { self.dst }
+
+    pub fn set_weight(&mut self, new_weight: f64) -> bool { 
+        self.weight = new_weight;
+        true
     }
 
-    pub fn get_dst(&self) -> Node {
-        self.dst
+    pub fn set_src(&mut self, new_src: &'a Node) -> bool {
+        self.src = new_src;
+        true
     }
+
+    pub fn set_dst(&mut self, new_dst: &'a Node) -> bool {
+        self.dst = new_dst;
+        true
+    }
+
 }
